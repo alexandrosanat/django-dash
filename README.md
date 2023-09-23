6ef905acb09c240546443700d0dd6b9dde50347c
alexandros <alexandrosanat@gmail.com>
2023-09-23 09:38:04 +0100

    Adds bootstrap and updates static

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django-dash
+
+Bootstrap theme: https://startbootstrap.com/theme/sb-admin-2
--- django_dash/settings.py
+++ django_dash/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Add all this after bringing over css/img/js/scss files under the project dir
+STATICFILES_LOCATION = "static"
+STATIC_ROOT = "static"
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "django_dash/static")]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
